5eb62bf4d187eae0e6950d59649a044e2ab8e6a5
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2019-03-14 12:34:04 -0400

    updates

--- src/main/java/com/codedifferently/genius/GeniusApplication.java
+++ src/main/java/com/codedifferently/genius/GeniusApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GeniusApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GeniusApplication.class, args);
+        SpringApplication.run(GeniusApplication.class);
     }
 
 }
